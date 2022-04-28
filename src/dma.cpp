0427e835dfaf28faf4cf30ea2e0e97921a105578
Sami Batuhan Basmaz Ölmez <repelliuss@gmail.com>
2022-04-29 02:03:48 +0300

    add notes about dma transfers

--- src/dma.cpp
+++ src/dma.cpp
@@ -1,6 +1,6 @@
 #include "dma.hpp"
-#include "data.hpp"
 #include "bits.hpp"
+#include "data.hpp"
 
 #include <assert.h>
 
@@ -11,7 +11,7 @@ bool direction_from_ram(const DMA::ChannelView &chview) {
   return bit(chview.channel_control, 0) != 0;
 }
 
-u32 increment(const DMA::ChannelView &chview) {
+u32 addr_step(const DMA::ChannelView &chview) {
   return (bit(chview.channel_control, 1) != 0) ? -4 : 4;
 }
 
@@ -78,6 +78,20 @@ void sync(const DMA::ChannelView &chview) {
   store32(chview.channel_control_addr, chview.channel_control, 0);
 }
 
+// NOTE: same as transfer_block_size() but used in SyncMode::manual
+u32 transfer_word_count(const DMA::ChannelView &chview) {
+  return bits_in_range(chview.block_control, 0, 15);
+}
+
+// NOTE: same as transfer_word_count() but used in SyncMode::request
+u32 transfer_block_size(const DMA::ChannelView &chview) {
+  return bits_in_range(chview.block_control, 0, 15);
+}
+
+u32 transfer_block_count(const DMA::ChannelView &chview) {
+  return bits_in_range(chview.block_control, 16, 31);
+}
+
 } // namespace
 } // namespace chview
 
@@ -86,6 +100,12 @@ namespace {
 
 using transfer_value_generator = u32 (*)(u32, u32);
 
+// REVIEW: make it a variable?
+// TODO: It seems that this value depends on the ram configuration. We are
+// currently using 2MB ram (0x200000) so we can mask all ram addresses with
+// 0x1fffff. But when 8MB ram is used (0x800000) this mask should be (0x7ffffc);
+constexpr u32 ram_addr_align_mask() { return 0x1ffffc; }
+
 constexpr u32 mask_reg_index_to_chview_type_val(u32 reg_index) {
   return reg_index & 0xfffffff0;
 }
@@ -93,28 +113,93 @@ constexpr u32 mask_reg_index_to_chview_type_val(u32 reg_index) {
 constexpr u32 get_otc_channel_transfer_value(u32 remaining_size, u32 addr) {
   // REVIEW: this value is for end of linked list but a specific loop could be
   // written for OTC which would remove this check
+  // REVIEW: looping by decrementing so 1 is valid, is there a more beautiful way?
   if (remaining_size == 1) {
     return 0xffffff;
   }
 
-  return (addr - 4) & 0x1fffff;
+  return (addr - 4) & ram_addr_align_mask();
 }
 
-// TODO: not quite right, see duckstation and nocash
-int transfer_block(const DMA &dma, DMA::ChannelView &chview) {
-  u32 increment = chview::increment(chview);
-  u32 addr = chview.base_address;
-  u32 remaining_size;
-  transfer_value_generator generator;
-
-  switch (chview.type) {
+transfer_value_generator get_generator(DMA::ChannelView::Type type) {
+  switch (type) {
   case DMA::ChannelView::Type::OTC:
-    generator = get_otc_channel_transfer_value;
-    break;
+    return get_otc_channel_transfer_value;
   default:
-    printf("Unhandled DMA source port %d\n", chview.type);
-    return -1;
+    printf("Unhandled DMA source port %d\n", type);
+    return nullptr;
   }
+}
+
+constexpr u32 get_otc_channel_transfer_value_see_note_about_transfer_manual(
+    u32 remaining_size, u32 aligned_addr) {
+  if (remaining_size == 1) {
+    return 0xffffff;
+  }
+
+  return (aligned_addr - 4);
+}
+
+transfer_value_generator
+get_generator_see_note_about_transfer_manual(DMA::ChannelView::Type type) {
+  switch (type) {
+  case DMA::ChannelView::Type::OTC:
+    return get_otc_channel_transfer_value_see_note_about_transfer_manual;
+  default:
+    printf("Unhandled DMA source port %d\n", type);
+    return nullptr;
+  }
+}
+
+// REVIEW: so this is my implementation of manual transfer. simias does manual
+// and request mode on the same function and I think it is wrong by code path
+// but also by means of implementation because it doesn't seem to take account
+// of block sizes being different than 4(?). I plan to split manual and request
+// too. Another thing is that, and also valid for duckstation, they seem to be
+// uselessly masking to align address. They mask at the start to align for 32bit
+// access and address seems to be staying multiples of 4 because in manual mode
+// increment is either 4 or -4 and in request mode block size gets multiplied
+// with 4 or -4 so it is still aligned. they also always mask for OTC channel
+// transfer value and my point is valid for there too. I dumped ram with both
+// implementations and diff results with nothing. Yet, to follow simias's guide,
+// I leave this here with an extra function for OTC channel get value.
+int transfer_manual(const DMA &dma, DMA::ChannelView &chview) {
+  const u32 addr_mask = ram_addr_align_mask();
+  const u32 step = chview::addr_step(chview);
+  const transfer_value_generator generator =
+      get_generator_see_note_about_transfer_manual(chview.type);
+  const u32 word_count = chview::transfer_word_count(chview);
+  u32 cur_addr = chview.base_address & addr_mask;
+
+  if (generator == nullptr)
+    return -1;
+
+  if (chview::direction_from_ram(chview)) {
+    printf("Unhandled DMA direction\n");
+    return -1;
+  } else {
+
+    for (u32 i = word_count; i > 0; --i) {
+      u32 value = generator(i, cur_addr);
+      memcpy(dma.ram.data + cur_addr, &value, sizeof(value));
+      cur_addr += step;
+    }
+  }
+
+  chview::finalize_transfer(chview);
+
+  return 0;
+}
+
+// TODO: not quite right, see transfer_manual and its review
+int transfer_manual_and_request(const DMA &dma, DMA::ChannelView &chview) {
+  const u32 increment = chview::addr_step(chview);
+  const transfer_value_generator generator = get_generator(chview.type);
+  u32 cur_addr = chview.base_address;
+  u32 remaining_size;
+
+  if (generator == nullptr)
+    return -1;
 
   if (chview::transfer_size(remaining_size, chview) < 0) {
     printf("Couldn't figure out DMA block transfer size\n");
@@ -126,15 +211,15 @@ int transfer_block(const DMA &dma, DMA::ChannelView &chview) {
     return -1;
   } else {
     while (remaining_size > 0) {
-      // NOTE: it seems like addr may be bad
-      //	 duckstation handles similar to this
-      // TODO: shouldn't be 0xfffffc ?
-      u32 cur_addr = addr & 0x1ffffc;
-      u32 src_word = generator(remaining_size, cur_addr);
+      // NOTE: it seems like addr may be bad duckstation handles similar to
+      // this
+      u32 aligned_addr = cur_addr & ram_addr_align_mask();
+      u32 src_word = generator(remaining_size, aligned_addr);
 
-      store32(dma.ram.data, src_word, cur_addr);
+      // load 32bit directly
+      memcpy(dma.ram.data + aligned_addr, &src_word, sizeof(src_word));
 
-      addr += increment;
+      cur_addr += increment;
       --remaining_size;
     }
   }
@@ -147,11 +232,10 @@ int transfer_block(const DMA &dma, DMA::ChannelView &chview) {
 int transfer(const DMA &dma, DMA::ChannelView &chview) {
   switch (chview::sync_mode(chview)) {
   case DMA::ChannelView::SyncMode::linked_list:
-    printf("Linked list mode unsupported\n");
     return -1;
 
   default:
-    return transfer_block(dma, chview);
+    return transfer_manual_and_request(dma, chview);
   }
 
   return 0;
